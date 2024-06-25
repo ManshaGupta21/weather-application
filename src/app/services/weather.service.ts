86868f712b6b2a20143881330d69edda0234790d
Mansha Gupta <mansha.gupta2109@gmail.com>
2024-06-25 19:01:14 +0530

    Initial Commit

---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+ 
+
+  constructor(private http: HttpClient) { }
+
+  getWeatherData(cityName:string):Observable<WeatherData>{
+    return this.http.get<WeatherData>(environment.weatherApiBaseUrl+'/city/'+cityName, {
+      headers: new HttpHeaders()
+      .set(environment.XRapidAPIHostHeaderName, environment.XRapidAPIHostHeaderValue)
+      .set(environment.XRapidAPIKeyHeaderName,environment.XRapidAPIKeyHeaderValue),
+    });
+}
+}
